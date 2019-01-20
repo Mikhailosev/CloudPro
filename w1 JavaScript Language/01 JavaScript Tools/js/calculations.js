68eb5166236bc3a0c3469d316569b89dc6a2f314
Mikhail <mikhail.losev2@gmail.com>
2019-01-20 04:38:12 +0300

    JavaScript week1 done

--- w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
@@ -1,12 +1,13 @@
-function multiplyBy()
-{
-// check the input
-    
+function multiplyBy() {
+    let a = document.getElementById('firstNumber').value;
+    let b = document.getElementById('secondNumber').value;
+        document.getElementById("result").innerHTML = a * b;
 }
 
-function divideBy() 
-{ 
-// check the input
- 
-}
+function divideBy() {
+    let a = document.getElementById('firstNumber').value;
+    let b = document.getElementById('secondNumber').value;
 
+    document.getElementById("result").innerHTML = a / b;
+
+}
--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,23 +1,50 @@
+var attempts = 1;
 let targetNumber = Math.floor(Math.random() * 10) + 1;
 
-function init () {
-	console.log("You have to code this!");
+var submit = document.getElementById('submit'),
+   number = document.getElementById('guess'),
+   input = document.getElementById('input'),
+   text = document.getElementById('text'),
+   replay = document.getElementById('replay');
+
+function init() {
+   submit.onclick= function(arg){
+      arg.preventDefault();
+      check(guess);
+    }
 }
 
-function check (value) {
-  
+function check(input) {
+   if (input.value == targetNumber) {
+      showWin(targetNumber);
+   } else if (input.value != targetNumber && attempts < 5) {
+      showError();
+   } else {
+      showLoss();
+   }
 }
 
-function showWin () {
-  
+function showWin(win) {
+   text.innerHTML = win + ' WOW! You WON!';
+
+   removeform()
 }
 
-function showError () {
-  
+function showError() {
+
+   text.innerHTML = attempts + ' unsuccsessful attempts. Choose number from 1 to 10!';
+attempts++
 }
 
-function showLoss () {
-  
+function showLoss() {
+   text.innerHTML ='You lose! The winning number was:'+targetNumber;
+   attempts++;
+   removeform()
+}
+
+function removeform() {
+   var element = document.getElementById("input");
+   element.parentNode.removeChild(element);
 }
 
 init();
