46dc3541cbca4120829b10fd475ffa73e9f841a1
iosadmin <iosadmin@mtk-idev-mba-05.wlan.metropolia.fi>
2019-01-30 10:54:59 +0200

    w3 1

--- w3 Vue/01 Introduction/logic.js
+++ w3 Vue/01 Introduction/logic.js
@@ -1,29 +1,54 @@
+let ID = -1;
+
 function isNotNumberValue(value) {
     return isNaN(value) || !isFinite(value);
 }
 var calc = new Vue({
-    el: "#app",
-    data: { x: 0, y: 0, finRes = 0 },
-    computing: {
-        div: function() {
+    el: '#app',
+    data: { x: 0, y: 0, lastResult: 0 },
+    computed: {
+        result: function() {
+
+            return this.lastResult;
+        }
+    },
+    methods: {
+        divideBy: function() {
             let x = parseFloat(this.x)
             if (isNotNumberValue(x))
-                return this.finRes;
+                return this.lastResult;
             let y = parseFloat(this.y)
             if (isNotNumberValue(y))
-                return this.finRes;
-            this.finRes = x / y
-            return this.finRes
+                return this.lastResult;
+
+            var calc = { number1: x, number2: y, operation: x / y }
+            ID++
+            localStorage.setItem(ID, JSON.stringify(calc))
+            console.log('updted local storage: ');
+            for (id = 0; id <= ID; id++) {
+                var log = (JSON.parse(localStorage.getItem(id) || '[]'))
+                console.log(log)
+            };
+            return this.lastResult = x / y
+
         },
-        mul: function() {
+        multiplyBy: function() {
             let x = parseFloat(this.x)
             if (isNotNumberValue(x))
-                return this.finRes;
+                return this.lastResult;
             let y = parseFloat(this.y)
             if (isNotNumberValue(y))
-                return this.finRes;
-            this.finRes = x * y
-            return this.finRes
+                return this.lastResult;
+            var calc = { number1: x, number2: y, operation: x * y }
+            ID++
+            localStorage.setItem(ID, JSON.stringify(calc))
+            console.log('updted local storage: ');
+            for (id = 0; id <= ID; id++) {
+                var log = (JSON.parse(localStorage.getItem(id) || '[]'))
+                console.log(log)
+            };
+
+            return this.lastResult = x * y
         }
     }
 });
